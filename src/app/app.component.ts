cbb7589e4f80aeb4d467d2e78011f4368fe4385f
Saniya15 <Saniyafirdous1508@gmail.com>
2022-09-23 10:48:36 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+declare function func():any;
+//declare function greet():any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'GettingStarted';
+  ngOnInit(): void
+  {
+    func();
+  }
+  
 }
